350460315038bf6a53e85e635941ab72d5b9a42c
Michael Ore <selbram1993@gmail.com>
2009-07-08 11:43:18 -0500

    Minor edits.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,48 +1,47 @@
 class PhotosController < ApplicationController
     def index
-        @photos = Photo.find(:all)
+	@categories = Category.find(:all)
 
-        respond_to do |format|
-            format.html
-            format.xml {render :xml => @photos}
-        end
+	respond_to do |format|
+	    format.html
+	end
     end
 
     def show
-        @photo = Photo.find(params[:id])
-        
-        respond_to do |format|
-            format.html
-            format.xml {render :xml => @photos}
-        end
+	@photo = Photo.find(params[:id])
+
+	respond_to do |format|
+	    format.html
+	    format.xml {render :xml => @photos}
+	end
     end
 
     def new
-        redirect_to :action => "index" unless session[:logged_in]
-        @photo = Photo.new
+	redirect_to :action => "index" unless session[:logged_in]
+	@photo = Photo.new
 
-        respond_to do |format|
-            format.html
-            format.xml {render :xml => @photo
-        end
+	respond_to do |format|
+	    format.html
+	    format.xml {render :xml => @photo
+	end
     end
 
     def edit
-        redirect_to :action => "index" unless session[:logged_in]
-        @photo = Photo.find(params[:id])
+	redirect_to :action => "index" unless session[:logged_in]
+	@photo = Photo.find(params[:id])
     end
 
     def create
-        @photo = Photo.new(params[:photo])
+	@photo = Photo.new(params[:photo])
 
-        respond_to do |format|
-            if @photo.save
-                flash[:notice] = 'Photo was successfully created.'
-                format.html { redirect_to(@photo) }
-                format.xml  { render :xml => @photo, :status => :created, :location => @photo }
-            else
-                format.html { render :action => "new" }
-                format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
-            end
-        end
+	respond_to do |format|
+	    if @photo.save
+		flash[:notice] = 'Photo was successfully created.'
+		format.html { redirect_to(@photo) }
+		format.xml  { render :xml => @photo, :status => :created, :location => @photo }
+	    else
+		format.html { render :action => "new" }
+		format.xml  { render :xml => @photo.errors, :status => :unprocessable_entity }
+	    end
+	end
 
