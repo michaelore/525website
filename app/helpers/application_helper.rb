3c9c4d7c5a03838cccbbcc3223589c5eb0c3b52b
Michael Ore <selbram1993@gmail.com>
2009-07-09 18:19:54 -0500

    Added method that is supposed to display thumbnails.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+    def display_thumb(photo)
+        link_to (image_tag ("thumb_" + photo.image)), photo
+    end
 end
