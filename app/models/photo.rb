1fdd807c2e42949e5db5e9db2df71462a08ca835
Michael Ore <selbram1993@gmail.com>
2009-07-13 18:56:38 -0500

    Photos are almost displayed properly now.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,6 +7,8 @@ class Photo < ActiveRecord::Base
         directory = 'public/images/data'
         path = File.join(directory, name)
         File.open(path, "wb") { |f| f.write(photo['image'].read) }
-        old_save
+        photo['image'] = 'data/' + name
+        new_photo = Photo.new(photo)
+        new_photo.old_save
     end
 end
