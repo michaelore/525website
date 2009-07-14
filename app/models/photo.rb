d3646efafb12e57d998ace56d6675ab93cf039bf
Michael Ore <selbram1993@gmail.com>
2009-07-13 21:53:50 -0500

    Got thumbnails to work and changed directory where photos are stored.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,13 +1,17 @@
 class Photo < ActiveRecord::Base
+    include Magick
     validates_presence_of :title, :image, :category_id
     belongs_to :category
     alias old_save save
     def save(photo)
         name = photo['image'].original_filename
-        directory = 'public/images/data'
+        directory = 'public/images'
         path = File.join(directory, name)
         File.open(path, "wb") { |f| f.write(photo['image'].read) }
-        photo['image'] = 'data/' + name
+        thumb = Image.read(path).first.thumbnail(70, 70)
+        thumb_path = File.join(directory, ("thumb_" + name))
+        thumb.write(thumb_path)
+        photo['image'] = name
         new_photo = Photo.new(photo)
         new_photo.old_save
     end
